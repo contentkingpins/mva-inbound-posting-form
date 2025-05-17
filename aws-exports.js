b848afa5f779881e0c5aabb30efa0085494420da
AnarchoFatSats <anarchosats@gmail.com>
2025-05-16 20:42:52 -0400

    Add AWS Amplify configuration files

---
+++ aws-exports.js
@@ -0,0 +1,9 @@
+// AWS Exports configuration
+const awsmobile = {
+    "aws_project_region": "us-east-1",
+    "aws_appsync_graphqlEndpoint": "",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY"
+};
+
+export default awsmobile; 
