a0334e1744206fd219c94a2049aa20e968ab0945
Your Name <your-email@example.com>
2025-05-20 09:16:30 -0700

    Fix Cognito authentication configuration issues

--- aws-exports.js
+++ aws-exports.js
@@ -1,9 +1,18 @@
 // AWS Exports configuration
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_appsync_graphqlEndpoint": "",
-    "aws_appsync_region": "us-east-1",
-    "aws_appsync_authenticationType": "API_KEY"
+    "aws_cognito_identity_pool_id": "",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_Lhc964tLD",
+    "aws_user_pools_web_client_id": "357mvn7k2nkjhl5nsq8h00o9tr",
+    "oauth": {},
+    "aws_cloud_logic_custom": [
+        {
+            "name": "leads-api",
+            "endpoint": "https://nv01uveape.execute-api.us-east-1.amazonaws.com/prod",
+            "region": "us-east-1"
+        }
+    ]
 };
 
 export default awsmobile; 
