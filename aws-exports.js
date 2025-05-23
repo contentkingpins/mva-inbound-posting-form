eae6dcef49dd406aac183a0478c4651a5782640a
Your Name <your-email@example.com>
2025-05-22 17:33:24 -0700

    Fix authentication redirect loop - Update dashboard cognito-auth.js and aws-exports.js to use correct UserPoolId case and ClientId (5t6mane4fnvineksoqb4ta0iu1)

--- aws-exports.js
+++ aws-exports.js
@@ -3,8 +3,8 @@ const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_identity_pool_id": "",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_Lhc964tLD",
-    "aws_user_pools_web_client_id": "357mvn7k2nkjhl5nsq8h00o9tr",
+    "aws_user_pools_id": "us-east-1_lhc964tLD",
+    "aws_user_pools_web_client_id": "5t6mane4fnvineksoqb4ta0iu1",
     "oauth": {},
     "aws_cloud_logic_custom": [
         {
